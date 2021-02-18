21b71f29c7371464bfdf7350962fdee7061cfeef
Nani2010 <n.f.alali@gmail.com>
2021-02-19 02:08:56 +0300

    first commit

--- Rade3/urls.py
+++ Rade3/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Rade3App.urls'))
+    path('Rade3App/', include('Rade3App.urls'))
 ]
--- Rade3App/views.py
+++ Rade3App/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+
+@api_view(['GET'])
+def Rade3Api (request):
+
+    api_urls ={
+        'Requests': '/reqeusts-list/',
+        'Request Detail': '/request-detail/<str:pk>',
+        'Create Request': '/request-create/',
+        'Update Request': '/request-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    } 
+    return Response(api_urls)
