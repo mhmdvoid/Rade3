e2a8f3218dcb7a73ebb5bd2d65e23a3e9129f95a
Nani2010 <n.f.alali@gmail.com>
2021-02-19 14:08:33 +0300

    second commit

--- Rade3App/models.py
+++ Rade3App/models.py
@@ -2,18 +2,31 @@ from django.db import models
 
 # Create your models here.
 class Victim_Info (models.Model):
-
+#  كٌ اسمك وعمرك إيميلك أو رقم جوالك أو حسابك في شبكات التواصل
+    
     name = models.CharField(max_length=200)
     age = models.PositiveSmallIntegerField(null=True, blank=True)
     email = models.EmailField(max_length=254)
+    #كيف ضايقك؟
+#(اشرح لي نوع الإيذاء وهل شتمكم أو هددكم أو طلب مال أو أجبركم على فعل معين..)
+
     description = models.TextField()
+   #في أي برنامج إلكتروني ضايقك؟
+#(اسم اللعبة أو التطبيق أو شبكة التواصل الاجتماعي مع رابطها)
+
     program = models.TextField()
+  #  ممكن تصورلنا الشاشة؟ - رفع مرفقات
+#(أحتاج منك أي دليل ضده) 
+
     files = models.ImageField(null = True)
+#مين مضايقك؟
+#اسمه أو جواله أو ايميله أو اسم حسابه مع الرابط)
 
     blackmailer_name = models.CharField(max_length=200, null=True, blank=True)
     blackmailer_email = models.EmailField(max_length=254 , null=True, blank=True)
     blackmailer_account = models.CharField(max_length=200, null=True, blank=True)
 
+    Request_Status = models.oneToMany(Victim_Request_Status)
 
   
     def __str__(self):
@@ -21,25 +34,21 @@ class Victim_Info (models.Model):
 
 class Victim_Request_Status(models.Model):
     
-    vid =  models.TextField()
+    # تاريخ الشكوى
     create_date = models.DateField(auto_now=False, auto_now_add=True)
+   # استيفاء بيانات الشكوى
     approved = models.BooleanField(null=True)
-    completed = models.BooleanField()
-    reviewr_name = models.CharField(max_length=200)
+    #التواصل مع المعتدي
+    contact_blackmailer = models.BooleanField()
+    #انقضاء المهلة
+    timeـout = models.BooleanField()
+    # النتيجه النهائيه
+    results = models.TextField()
 
     def __str__(self):
         return self.completed
 
-class Reviewr_Action (models.Model):
-    
-    vid =  models.TextField()
-    create_date = models.DateField(auto_now=False, auto_now_add=True)
-    description = models.TextField()
-    reviewr_name  = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.description
-
+#نصائح للتغلب على المعتدين
 
 class Resources(models.Model):
         
